e95ce0f79817481bb74a8cc91afb4a14694194a6
Siva Rama Krishna Prasad Changala <123384127+cleancoder1016@users.noreply.github.com>
2023-06-29 08:34:59 -0400

    create crop.py

---
+++ crop.py
@@ -0,0 +1,16 @@
+#set transparent to true
+transparent = True
+
+#set transparency to 2
+transparency = 2
+
+#set is_cube to false
+is_cube = False
+
+#set glass to false
+glass = False
+
+#set translucent to false
+translucent = False
+
+colliders = []
