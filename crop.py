413f4803caee239b92e3570868ffe911c58fa033
Siva Rama Krishna Prasad Changala <123384127+cleancoder1016@users.noreply.github.com>
2023-06-29 09:29:07 -0400

    Update crop.py with test cases

--- crop.py
+++ crop.py
@@ -51,3 +51,39 @@ shading_values = [
     [0.8, 0.8, 0.8, 0.8],
     [0.8, 0.8, 0.8, 0.8],
 ]
+
+class minecraftCropTestCases(unittest.TestCase):
+    def test_crop_properties(self):
+        # Test transparency property
+        assert transparent is True
+
+        # Test transparency value
+        assert transparency == 2
+
+        # Test is_cube property
+        assert is_cube is False
+
+        # Test glass property
+        assert glass is False
+
+        # Test translucent property
+        assert translucent is False
+
+    def test_crop_data_structures(self):
+        # Test vertex_positions list
+        assert len(vertex_positions) == 8
+        assert len(vertex_positions[0]) == 12
+
+        # Test tex_coords list
+        assert len(tex_coords) == 8
+        assert len(tex_coords[0]) == 12
+
+        # Test shading_values list
+        assert len(shading_values) == 8
+        assert len(shading_values[0]) == 4
+
+
+# Run the test cases
+if __name__ == '__main__':
+    unittest.main()
+
