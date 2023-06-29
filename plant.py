c0a315c52056a41d74ffac353cded84684e736ae
kentruslin <kentruslin@yahoo.com>
2023-06-29 13:21:25 -0400

    implementing issue#15

---
+++ plant.py
@@ -0,0 +1,142 @@
+# import module to write testcases for the code
+import unittest
+# variables declarations
+transparent = True
+transparency = 2
+translucent = False
+is_cube = False
+glass = False
+
+colliders = []
+# vertices positions
+vertex_positions = [
+	[-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],
+	[-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],
+]
+# textures coordinates
+tex_coords = [
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+]
+
+# shading values
+shading_values = [
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+]
+
+class MyCodeTestCase(unittest.TestCase):
+    def setUp(self):
+        # Set up any necessary test data or configurations
+
+        # Initialize the code variables for testing
+        self.transparent = True
+        self.transparency = 2
+        self.is_cube = False
+        self.glass = False
+        self.translucent = False
+        self.colliders = []
+
+        self.vertex_positions = [
+	[-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],
+	[-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],
+	[ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],
+]
+      
+ 
+        self.tex_coords = [
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+]
+
+        self.shading_values = [
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+	[1.0, 1.0, 1.0, 1.0],
+]
+
+
+
+    def test_transparent(self):
+        self.assertTrue(self.transparent)
+
+    def test_transparency(self):
+        self.assertEqual(self.transparency, 2)
+
+    def test_is_cube(self):
+        self.assertFalse(self.is_cube)
+
+    def test_glass(self):
+        self.assertFalse(self.glass)
+
+    def test_translucent(self):
+        self.assertFalse(self.translucent)
+
+    def test_colliders(self):
+        self.assertIsInstance(self.colliders, list)
+
+    def test_vertex_positions(self):
+        for positions in self.vertex_positions:
+            self.assertIsInstance(positions, list)
+            self.assertEqual(len(positions), 12)
+
+    def test_tex_coords(self):
+        for coords in self.tex_coords:
+            self.assertIsInstance(coords, list)
+            self.assertEqual(len(coords), 12)
+
+    def test_shading_values(self):
+        for values in self.shading_values:
+            self.assertIsInstance(values, list)
+            self.assertEqual(len(values), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
