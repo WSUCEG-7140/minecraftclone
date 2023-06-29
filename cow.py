277e14d7592edb89623559f7a113de48df6552af
Siva Rama Krishna Prasad Changala <123384127+cleancoder1016@users.noreply.github.com>
2023-06-29 08:25:04 -0400

    create cow.py

---
+++ cow.py
@@ -0,0 +1,16 @@
+transparent = True  # Indicates whether the cow model is transparent
+
+transparency = 2  # The transparency level of the cow model
+
+is_cube = False  # Indicates whether the cow model is a cube shape
+
+glass = False  # Indicates whether the cow model has a glass material
+
+translucent = False  # Indicates whether the cow model is translucent
+
+colliders = [
+    [
+        (-0.4375, -0.5, -0.4375),
+        (0.4375, 0.5, 0.4375)
+    ]
+]  # Defines the collision boundaries of the cow model
