b16b6d6bec6731893d9841718e087e1702b5ecde
kentruslin <kentruslin@yahoo.com>
2023-06-30 00:56:52 -0400

    implementing issue#15

--- fire.py
+++ fire.py
@@ -34,7 +34,7 @@ shading_values = [
 ]
 
 
-
+# Class Unittest
 class MyCodeTestCase(unittest.TestCase):
     def setUp(self):
         # Set up any necessary test data or configurations
@@ -70,35 +70,35 @@ class MyCodeTestCase(unittest.TestCase):
         ]
 
 
-
+    # test transparent variable
     def test_transparent(self):
         self.assertTrue(self.transparent)
-
+    # test transparency variable
     def test_transparency(self):
         self.assertEqual(self.transparency, 2)
-
+    # test is_cube variable
     def test_is_cube(self):
         self.assertFalse(self.is_cube)
-
+    # test glass variable
     def test_glass(self):
         self.assertFalse(self.glass)
-
+    # test translucent variable
     def test_translucent(self):
         self.assertFalse(self.translucent)
-
+    # test colliders variable
     def test_colliders(self):
         self.assertIsInstance(self.colliders, list)
-
+    # test vertices positions variable
     def test_vertex_positions(self):
         for positions in self.vertex_positions:
             self.assertIsInstance(positions, list)
             self.assertEqual(len(positions), 12)
-
+    # test texture coordinates variable
     def test_tex_coords(self):
         for coords in self.tex_coords:
             self.assertIsInstance(coords, list)
             self.assertEqual(len(coords), 12)
-
+    # test shading values variable
     def test_shading_values(self):
         for values in self.shading_values:
             self.assertIsInstance(values, list)
