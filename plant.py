9dc3a099cf8fc85088ff2f8a32ce3a2060bd1535
kentruslin <kentruslin@yahoo.com>
2023-06-29 23:48:43 -0400

    implementing issue#15

--- plant.py
+++ plant.py
@@ -30,7 +30,7 @@ shading_values = [
 	[1.0, 1.0, 1.0, 1.0],
 	[1.0, 1.0, 1.0, 1.0],
 ]
-
+# Class Unittest
 class MyCodeTestCase(unittest.TestCase):
     def setUp(self):
         # Set up any necessary test data or configurations
@@ -42,7 +42,7 @@ class MyCodeTestCase(unittest.TestCase):
         self.glass = False
         self.translucent = False
         self.colliders = []
-
+        # Initialize vertices position variables for testing 
         self.vertex_positions = [
 	[-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],
 	[-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],
@@ -50,14 +50,14 @@ class MyCodeTestCase(unittest.TestCase):
 	[ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],
 ]
       
- 
+        # Initialize texture coordinates variables for testing
         self.tex_coords = [
 	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
 	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
 	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
 	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],
 ]
-
+        # Initialize shading values variables for testing
         self.shading_values = [
 	[1.0, 1.0, 1.0, 1.0],
 	[1.0, 1.0, 1.0, 1.0],
@@ -66,35 +66,35 @@ class MyCodeTestCase(unittest.TestCase):
 ]
 
 
-
+    # test transparent variable
     def test_transparent(self):
         self.assertTrue(self.transparent)
-
+    # test transparency variable
     def test_transparency(self):
         self.assertEqual(self.transparency, 2)
-
+    # test is_cube variable
     def test_is_cube(self):
         self.assertFalse(self.is_cube)
-
+    # test glass variable
     def test_glass(self):
         self.assertFalse(self.glass)
-
+    # test translucent variable
     def test_translucent(self):
         self.assertFalse(self.translucent)
-
+    # test colliders variable
     def test_colliders(self):
         self.assertIsInstance(self.colliders, list)
-
+    # test vertex_positions variable
     def test_vertex_positions(self):
         for positions in self.vertex_positions:
             self.assertIsInstance(positions, list)
             self.assertEqual(len(positions), 12)
-
+    # test tex_coords
     def test_tex_coords(self):
         for coords in self.tex_coords:
             self.assertIsInstance(coords, list)
             self.assertEqual(len(coords), 12)
-
+    # test shading_values
     def test_shading_values(self):
         for values in self.shading_values:
             self.assertIsInstance(values, list)
