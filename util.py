0784b570560c67686500a704aee7d2746e2b3495
kentruslin <kentruslin@yahoo.com>
2023-06-30 00:44:35 -0400

    implementing issue#15

--- util.py
+++ util.py
@@ -16,12 +16,12 @@ UP = glm.ivec3(0, 1, 0)
 DOWN = glm.ivec3(0, -1, 0)
 SOUTH = glm.ivec3(0, 0, 1)
 NORTH = glm.ivec3(0, 0, -1)
-
+# Class Unittest
 class MyCodeTestCase(unittest.TestCase):
     def setUp(self):
         # Set up any necessary test data or configurations
 
-        # Initialize the code variables for testing
+        # Initialize the variables from OpenGL interface for testing
         self.DIRECTIONS = (glm.ivec3(1, 0, 0), 
             glm.ivec3(-1, 0, 0), 
             glm.ivec3(0, 1, 0), 
@@ -35,39 +35,39 @@ class MyCodeTestCase(unittest.TestCase):
         self.SOUTH = glm.ivec3(0, 0, 1)
         self.NORTH = glm.ivec3(0, 0, -1)
 
-        
+    # test EAST variable 
     def test_EAST(self):
         self.assertIsInstance(self.EAST, glm.ivec3)
         self.assertEqual(len(self.EAST), 3)
         self.assertEqual([self.EAST.x, self.EAST.y, self.EAST.z], [1, 0, 0])
-    
+    # test WEST variable
     def test_WEST(self):
         self.assertIsInstance(self.WEST, glm.ivec3)
         self.assertEqual(len(self.WEST), 3)
         self.assertEqual([self.WEST.x, self.WEST.y, self.WEST.z], [-1, 0, 0])
-
+    # test UP variable
     def test_UP(self):
         self.assertIsInstance(self.UP, glm.ivec3)
         self.assertEqual(len(self.UP), 3)
         self.assertEqual([self.UP.x, self.UP.y, self.UP.z], [0, 1, 0])
-
+    # test DOWN variable
     def test_DOWN(self):
         self.assertIsInstance(self.DOWN, glm.ivec3)
         self.assertEqual(len(self.DOWN), 3)
         self.assertEqual([self.DOWN.x, self.DOWN.y, self.DOWN.z], [0, -1, 0])
-
+    # test SOUTH variable
     def test_SOUTH(self):
         self.assertIsInstance(self.SOUTH, glm.ivec3)
         self.assertEqual(len(self.SOUTH), 3)
         self.assertEqual([self.SOUTH.x, self.SOUTH.y, self.SOUTH.z], [0, 0, 1])
-
+    # test NORTH variable
     def test_NORTH(self):
         #self.assertIsInstance(self.NORTH, glm.ivec3)
         self.assertEqual(len(self.NORTH), 3)
         self.assertEqual([self.NORTH.x, self.NORTH.y, self.NORTH.z], [0, 0, -1])
 
 
-
+    # test DIRECTIONS variable
     def test_DIRECTIONS(self):
         for coords in self.DIRECTIONS:
             self.assertEqual(len(coords), 3)
