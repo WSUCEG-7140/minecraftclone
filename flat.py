b96854048c8e60e25ab14e5e5a3def48a474a53c
Siva Rama Krishna Prasad Changala <123384127+cleancoder1016@users.noreply.github.com>
2023-07-05 12:33:22 -0400

    commented flat.py code

--- flat.py
+++ flat.py
@@ -6,16 +6,19 @@ translucent = False
 
 colliders = []
 
+#set values to vertext positions
 vertex_positions = [
 	[ 0.5, -0.4375,  0.5,   0.5, -0.4375, -0.5,  -0.5, -0.4375, -0.5,  -0.5, -0.4375,  0.5], # top
 	[-0.5, -0.4375,  0.5,  -0.5, -0.4375, -0.5,   0.5, -0.4375, -0.5,   0.5, -0.4375,  0.5], # bottom
 ]
 
+#set values to texture co-ordinates
 tex_coords = [
 	[0.0, 1.0, 0.0,  0.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 1.0, 0.0],
 	[0.0, 1.0, 0.0,  0.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 1.0, 0.0],
 ]
 
+#set shading values
 shading_values = [
 	[1.0, 1.0, 1.0, 1.0],
 	[0.4, 0.4, 0.4, 0.4],
