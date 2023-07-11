3ae5265700032cde35997262c2a6621973e8959a
Deekshitha-kancharla <133474110+Deekshitha-kancharla@users.noreply.github.com>
2023-07-11 01:11:41 -0400

    Update shader.py

--- shader.py
+++ shader.py
@@ -53,6 +53,118 @@ class Shader:
 
     def __del__(self):
         gl.glDeleteProgram(self.program)
+#test cases
+    class ShaderTestCase(unittest.TestCase):
+    def test_create_shader_success(self):
+        # Test case for successful shader compilation
+        target = gl.glCreateShader(gl.GL_VERTEX_SHADER)
+        source_path = "path/to/vertex_shader.vert"
+        
+        with patch("builtins.open", mock_open(read_data="shader_source_code")) as mock_file:
+            create_shader(target, source_path)
+        
+        gl.glCompileShader.assert_called_once_with(target)
+        gl.glGetShaderiv.assert_called_once_with(target, gl.GL_INFO_LOG_LENGTH, ctypes.byref(gl.GLint()))
+        gl.glGetShaderInfoLog.assert_not_called()
+
+    def test_create_shader_error(self):
+        # Test case for shader compilation error
+        target = gl.glCreateShader(gl.GL_VERTEX_SHADER)
+        source_path = "path/to/vertex_shader.vert"
+        
+        with patch("builtins.open", mock_open(read_data="shader_source_code")) as mock_file:
+            gl.glGetShaderiv.return_value = 1  # Error log length > 1
+            gl.glGetShaderInfoLog.return_value = b"Shader compilation error"
+            with self.assertRaises(Shader_error) as cm:
+                create_shader(target, source_path)
+            self.assertEqual(str(cm.exception), "b'Shader compilation error'")
+
+    def test_shader_init(self):
+        # Test case for Shader class initialization
+        vert_path = "path/to/vertex_shader.vert"
+        frag_path = "path/to/fragment_shader.frag"
+        
+        with patch.object(gl, "glCreateProgram") as mock_create_program, \
+                patch.object(gl, "glCreateShader") as mock_create_shader, \
+                patch.object(gl, "glAttachShader") as mock_attach_shader, \
+                patch.object(gl, "glLinkProgram") as mock_link_program, \
+                patch.object(gl, "glDeleteShader") as mock_delete_shader:
+            shader = Shader(vert_path, frag_path)
+            
+            mock_create_program.assert_called_once()
+            
+            mock_create_shader.assert_any_call(gl.GL_VERTEX_SHADER)
+            mock_create_shader.assert_any_call(gl.GL_FRAGMENT_SHADER)
+            
+            mock_attach_shader.assert_any_call(shader.program, shader.vert_shader)
+            mock_attach_shader.assert_any_call(shader.program, shader.frag_shader)
+            
+            mock_link_program.assert_called_with(shader.program)
+            
+            mock_delete_shader.assert_called_with(shader.vert_shader)
+            mock_delete_shader.assert_called_with(shader.frag_shader)
+
+    def test_shader_find_uniform(self):
+        # Test case for finding a uniform location
+        vert_path = "path/to/vertex_shader.vert"
+        frag_path = "path/to/fragment_shader.frag"
+        shader = Shader(vert_path, frag_path)
+        uniform_name = "projectionMatrix"
+        
+        with patch.object(gl, "glGetUniformLocation") as mock_get_uniform_location:
+            mock_get_uniform_location.return_value = 42
+            location = shader.find_uniform(uniform_name)
+            mock_get_uniform_location.assert_called_with(shader.program, ctypes.create_string_buffer(uniform_name.encode()))
+            self.assertEqual(location, 42)
+
+    def test_shader_uniform_matrix(self):
+        # Test case for setting a matrix uniform
+        vert_path = "path/to/vertex_shader.vert"
+        frag_path = "path/to/fragment_shader.frag"
+        shader = Shader(vert_path, frag_path)
+        location = 42
+        matrix = glm.mat4(1.0)
+        
+        with patch.object(gl, "glUniformMatrix4fv") as mock_uniform_matrix:
+            shader.uniform_matrix(location, matrix)
+            mock_uniform_matrix.assert_called_with(location, 1, gl.GL_FALSE, glm.value_ptr(matrix))
+
+    def test_shader_uniform_float(self):
+        # Test case for setting a float uniform
+        vert_path = "path/to/vertex_shader.vert"
+        frag_path = "path/to/fragment_shader.frag"
+        shader = Shader(vert_path, frag_path)
+        location = 42
+        value = 3.14
+        
+        with patch.object(gl, "glUniform1f") as mock_uniform_float:
+            shader.uniform_float(location, value)
+            mock_uniform_float.assert_called_with(location, value)
+
+    def test_shader_use(self):
+        # Test case for using the shader program
+        vert_path = "path/to/vertex_shader.vert"
+        frag_path = "path/to/fragment_shader.frag"
+        shader = Shader(vert_path, frag_path)
+        
+        with patch.object(gl, "glUseProgram") as mock_use_program:
+            shader.use()
+            mock_use_program.assert_called_with(shader.program)
+
+    def test_shader_stop(self):
+        # Test case for stopping the shader program
+        vert_path = "path/to/vertex_shader.vert"
+        frag_path = "path/to/fragment_shader.frag"
+        shader = Shader(vert_path, frag_path)
+        
+        with patch.object(gl, "glUseProgram") as mock_use_program:
+            shader.stop()
+            mock_use_program.assert_called_with(0)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
     def find_uniform(self, name):
         # Find the location of a uniform variable by name
