a7af71ef957973412db682a335cd0b184a7f89af
Siva Rama Krishna Prasad Changala <123384127+cleancoder1016@users.noreply.github.com>
2023-07-23 09:03:59 -0400

    Added test cases

--- liquid.py
+++ liquid.py
@@ -1,3 +1,4 @@
+import unittest
 # in the end, it'd be nice to have it so that liquids fill up the whole block when they have a block above them
 # this would avoid the problems this solution has
 transparent = True
@@ -37,3 +38,37 @@ shading_values = [
 	[0.8, 0.8, 0.8, 0.8],
 	[0.8, 0.8, 0.8, 0.8],
 ]
+class minecraftLiquidTestCases(unittest.TestCase):
+    def test_liquid_properties(self):
+        # Test transparency property
+        assert transparent is True
+
+        # Test transparency value
+        assert transparency == 1
+
+        # Test is_cube property
+        assert is_cube is True
+
+        # Test glass property
+        assert glass is True
+
+        # Test translucent property
+        assert translucent is True
+
+    def test_liquid_data_structures(self):
+        # Test vertex_positions list
+        assert len(vertex_positions) == 6
+        assert len(vertex_positions[0]) == 12
+
+        # Test tex_coords list
+        assert len(tex_coords) == 6
+        assert len(tex_coords[0]) == 12
+
+        # Test shading_values list
+        assert len(shading_values) == 6
+        assert len(shading_values[0]) == 4
+
+
+# Run the test cases
+if __name__ == '__main__':
+    unittest.main()
