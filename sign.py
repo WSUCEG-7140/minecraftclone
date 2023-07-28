bfbc4b2b421e1eb2bd1727d1c1420c6404789cee
Siva Rama Krishna Prasad Changala <123384127+cleancoder1016@users.noreply.github.com>
2023-07-28 09:48:31 -0400

    Update sign.py with added variables with feasible scope

--- sign.py
+++ sign.py
@@ -1,68 +1,40 @@
-import unittest
-# Set transparency-related properties
-transparent = True           # Indicates if the object is transparent
-transparency = 2             # Transparency level (adjust as needed)
-is_cube = False              # Indicates if the object is a cube (can be used for collision detection)
-glass = False                # Indicates if the object is made of glass (a specific type of transparency)
-translucent = False          # Indicates if the object is translucent (allows some light to pass through)
+class MinecraftSign:
+    def __init__(self):
+        """
+        Initialize the MinecraftSign class.
 
-# Initialize colliders list (can be used to store colliders for the object)
-colliders = []
+        This constructor initializes all the properties of the sign block.
+        """
+        # Set transparency-related properties
+        self.transparent = True           # Indicates if the object is transparent
+        self.transparency = 2             # Transparency level (adjust as needed)
+        self.is_cube = False              # Indicates if the object is a cube (can be used for collision detection)
+        self.glass = False                # Indicates if the object is made of glass (a specific type of transparency)
+        self.translucent = False          # Indicates if the object is translucent (allows some light to pass through)
 
-# Define vertex positions for the object
-vertex_positions = [
-	[-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],  # Face 1
-	[-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],  # Face 2
-	[ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],  # Face 3
-	[ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],  # Face 4
-]
+        # Initialize colliders list (can be used to store colliders for the object)
+        self.colliders = []
 
-# Define texture coordinates for the object
-tex_coords = [
-	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 1
-	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 2
-	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 3
-	[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 4
-]
+        # Define vertex positions for the object
+        self.vertex_positions = [
+            [-0.3536, 0.5000,  0.3536, -0.3536, -0.5000,  0.3536,  0.3536, -0.5000, -0.3536,  0.3536, 0.5000, -0.3536],  # Face 1
+            [-0.3536, 0.5000, -0.3536, -0.3536, -0.5000, -0.3536,  0.3536, -0.5000,  0.3536,  0.3536, 0.5000,  0.3536],  # Face 2
+            [ 0.3536, 0.5000, -0.3536,  0.3536, -0.5000, -0.3536, -0.3536, -0.5000,  0.3536, -0.3536, 0.5000,  0.3536],  # Face 3
+            [ 0.3536, 0.5000,  0.3536,  0.3536, -0.5000,  0.3536, -0.3536, -0.5000, -0.3536, -0.3536, 0.5000, -0.3536],  # Face 4
+        ]
 
-# Define shading values for the object
-shading_values = [
-	[1.0, 1.0, 1.0, 1.0],  # Face 1
-	[1.0, 1.0, 1.0, 1.0],  # Face 2
-	[1.0, 1.0, 1.0, 1.0],  # Face 3
-	[1.0, 1.0, 1.0, 1.0],  # Face 4
-]
-class minecraftSignTestCases(unittest.TestCase):
-    def test_sign_properties(self):
-        # Test transparency property
-        assert transparent is True
+        # Define texture coordinates for the object
+        self.tex_coords = [
+            [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 1
+            [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 2
+            [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 3
+            [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0],  # Face 4
+        ]
 
-        # Test transparency value
-        assert transparency == 2
-
-        # Test is_cube property
-        assert is_cube is False
-
-        # Test glass property
-        assert glass is False
-
-        # Test translucent property
-        assert translucent is False
-
-    def test_sign_data_structures(self):
-        # Test vertex_positions list
-        assert len(vertex_positions) == 4
-        assert len(vertex_positions[0]) == 12
-
-        # Test tex_coords list
-        assert len(tex_coords) == 4
-        assert len(tex_coords[0]) == 12
-
-        # Test shading_values list
-        assert len(shading_values) == 4
-        assert len(shading_values[0]) == 4
-
-
-# Run the test cases
-if __name__ == '__main__':
-    unittest.main()
+        # Define shading values for the object
+        self.shading_values = [
+            [1.0, 1.0, 1.0, 1.0],  # Face 1
+            [1.0, 1.0, 1.0, 1.0],  # Face 2
+            [1.0, 1.0, 1.0, 1.0],  # Face 3
+            [1.0, 1.0, 1.0, 1.0],  # Face 4
+        ]
